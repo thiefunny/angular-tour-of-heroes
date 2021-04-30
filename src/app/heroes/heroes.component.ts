8b78b9099cb4303c82c02cdad44ab90c1e5e649c
thiefunny <mikolaj@walanus.com>
2021-04-30 15:35:43 +0200

    added hero details component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import {HEROES} from '../mock.heroes';
+import { HEROES } from '../mock.heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
+
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
@@ -21,5 +22,4 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
 }
